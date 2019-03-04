63ff58e2179ba6883787853343b4387cc0aed52e
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2019-03-04 15:59:27 +0100

    Feature: Allowed using the editor instance as the only argument of `CKEditorInspector.attach()`. Closes #11.

--- src/app.js
+++ src/app.js
@@ -20,16 +20,49 @@ const container = document.createElement( 'div' );
 container.className = 'ck-inspector-wrapper';
 const inspectorRef = React.createRef();
 
+let editorCount = 0;
+
 export default class CKEditorInspector {
-	static attach( name, editor ) {
+	/**
+	 * Attaches the inspector to an editor instance.
+	 *
+	 *		ClassicEditor
+	 *			.create( ... )
+	 *			.then( editor => {
+	 *				CKEditorInspector.attach( editor );
+	 *
+	 *				// Alternatively:
+	 *				// CKEditorInspector.attach( 'my-editor', editor );
+	 *			} )
+	 *			.catch( error => {
+	 *				console.error( error );
+	 *			} );
+	 *
+	 * @param {Editor|String} editorOrName When an unique string is provided, the editor will be listed in the inspector
+	 * under a name (the instance passed as a second argument). If an editor instance is passed, the editor with be
+	 * attached and assigned a generated name.
+	 * @param {Editor} [editor] An instance of the editor, if the first argument was specified as a string.
+	 * @returns {String} The unique name of the editor in the inspector. Useful when using `CKEditorInspector.detach()`.
+	 */
+	static attach( editorOrName, editor ) {
+		let name, instance;
+
+		if ( typeof editorOrName === 'string' ) {
+			name = editorOrName;
+			instance = editor;
+		} else {
+			name = `editor-${ ++editorCount }`;
+			instance = editorOrName;
+		}
+
 		console.group('%cAttached the inspector to a CKEditor 5 instance. To learn more, visit https://ckeditor.com/docs/ckeditor5.',
 		'font-weight: bold;' );
-		console.log( `Editor instance "${ name }"`, editor );
+		console.log( `Editor instance "${ name }"`, instance );
 		console.groupEnd();
 
-		editors.set( name, editor );
+		editors.set( name, instance );
 
-		editor.on( 'destroy', () => {
+		instance.on( 'destroy', () => {
 			CKEditorInspector.detach( name );
 		} );
 
@@ -39,6 +72,8 @@ export default class CKEditorInspector {
 		}
 
 		CKEditorInspector._updateEditors( editors );
+
+		return name;
 	}
 
 	static detach( name ) {
