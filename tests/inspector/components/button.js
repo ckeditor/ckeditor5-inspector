90c320529119fe8a9e6c75a35333d5eb70bad26c
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2019-03-08 13:40:22 +0100

    Tests: Added tests for <Button />

--- tests/inspector/components/button.js
+++ tests/inspector/components/button.js
@@ -16,6 +16,32 @@ describe( '<Button />', () => {
 	it( 'renders a button', () => {
 		const wrapper = shallow( <Button /> );
 
-		expect( wrapper.find( '.ck-inspector-button' ) ).to.have.length( 1 );
+		expect( wrapper.first() ).to.have.className( 'ck-inspector-button' );
+		expect( wrapper.first() ).to.have.attr( 'type', 'button' );
+	} );
+
+	it( 'reacts to props#text', () => {
+		const wrapper = shallow( <Button /> );
+
+		wrapper.setProps( { text: 'foo' } );
+
+		expect( wrapper.first() ).to.have.attr( 'title', 'foo' );
+		expect( wrapper.first().contains( 'foo' ) ).to.be.true;
+	} );
+
+	it( 'reacts to props#type', () => {
+		const wrapper = shallow( <Button /> );
+
+		wrapper.setProps( { type: 'bar' } );
+
+		expect( wrapper.first() ).to.have.className( 'ck-inspector-button_bar' );
+	} );
+
+	it( 'executes props#onClick when clicked', () => {
+		const spy = sinon.spy();
+		const wrapper = shallow( <Button onClick={spy} /> );
+
+		wrapper.first().simulate( 'click' );
+		sinon.assert.callCount( spy, 1 );
 	} );
 } );
