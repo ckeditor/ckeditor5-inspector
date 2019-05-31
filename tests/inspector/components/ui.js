21161524bc901b0b6e89a9272c3692f0ba5f6edb
Kamil Piechaczek <pomek@users.noreply.github.com>
2019-05-31 11:08:11 +0200

    Replaced rendering function.

--- tests/inspector/components/ui.js
+++ tests/inspector/components/ui.js
@@ -38,7 +38,7 @@ describe( '<InspectorUI />', () => {
 			TestEditor.create( editor2Element )
 		] ).then( ( [ editor1, editor2 ] ) => {
 			editors = new Map( [ [ 'first', editor1 ], [ 'second', editor2 ] ] );
-			wrapper = mount(
+			wrapper = shallow(
 				<InspectorUI editors={editors} />,
 				{ attachTo: container }
 			);
@@ -70,7 +70,7 @@ describe( '<InspectorUI />', () => {
 		it( 'restores state#height from the storage and sets it to body', () => {
 			window.localStorage.setItem( 'ck5-inspector-height', '123px' );
 
-			const wrapper = mount(
+			const wrapper = shallow(
 				<InspectorUI editors={editors} />,
 				{ attachTo: container }
 			);
@@ -84,7 +84,7 @@ describe( '<InspectorUI />', () => {
 		it( 'restores state#isCollapsed from the storage', () => {
 			window.localStorage.setItem( 'ck5-inspector-is-collapsed', 'true' );
 
-			const wrapper = mount(
+			const wrapper = shallow(
 				<InspectorUI editors={editors} />,
 				{ attachTo: container }
 			);
@@ -97,7 +97,7 @@ describe( '<InspectorUI />', () => {
 		it( 'restores state#activeTab from the storage', () => {
 			window.localStorage.setItem( 'ck5-inspector-active-tab-name', 'Commands' );
 
-			const wrapper = mount(
+			const wrapper = shallow(
 				<InspectorUI editors={editors} />,
 				{ attachTo: container }
 			);
