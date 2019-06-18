4fcb6aa504fdd38990d443a88303c6b807e30790
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2019-06-18 10:59:06 +0200

    Code refactoring: simplified conditions in the normalizeArguments() helper.

--- src/utils.js
+++ src/utils.js
@@ -10,42 +10,22 @@ export function normalizeArguments( args ) {
 		editors: {}
 	};
 
-	// attach( editor );
-	// attach( { foo: editor1, bar: editor2 } );
-	if ( args.length === 1 ) {
-		const editorOrEditorPairs = args[ 0 ];
+	const editorOrEditorPairs = args[ 0 ];
 
-		// attach( editor );
-		if ( isEditorInstance( editorOrEditorPairs ) ) {
-			normalized.editors[ getNextEditorName() ] = args[ 0 ];
-		}
-		// attach( { foo: editor1, bar: editor2, ... } );
-		else {
-			for ( const name in editorOrEditorPairs ) {
-				normalized.editors[ name ] = editorOrEditorPairs[ name ];
-			}
+	// attach( editor );
+	if ( isEditorInstance( editorOrEditorPairs ) ) {
+		normalized.editors[ getNextEditorName() ] = args[ 0 ];
+	}
+	// attach( { foo: editor1, bar: editor2, ... } );
+	else {
+		for ( const name in editorOrEditorPairs ) {
+			normalized.editors[ name ] = editorOrEditorPairs[ name ];
 		}
 	}
+
 	// attach( editor, { options } );
 	// attach( { foo: editor1, bar: editor2 }, { options } );
-	else {
-		// attach( editor, { options } );
-		if ( isEditorInstance( args[ 0 ] ) ) {
-			normalized.editors[ getNextEditorName() ] = args[ 0 ];
-		}
-		// attach( { foo: editor1, bar: editor2 }, { options } );
-		else {
-			const editorOrEditorPairs = args[ 0 ];
-
-			for ( const name in editorOrEditorPairs ) {
-				normalized.editors[ name ] = editorOrEditorPairs[ name ];
-			}
-		}
-
-		normalized.options = args[ 1 ];
-	}
-
-	normalized.options = normalized.options || {};
+	normalized.options = args[ 1 ] || {};
 
 	return normalized;
 }
