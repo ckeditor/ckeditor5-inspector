1b51a427f5c98879ec0747ca4b86fce07ea8c6b7
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2020-03-25 12:59:04 +0100

    Updated commands pane tests.

---
+++ tests/inspector/commands/pane.js
@@ -0,0 +1,81 @@
+/**
+ * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+/* global document, window */
+
+import React from 'react';
+import TestEditor from '../../utils/testeditor';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import CommandsPane from '../../../src/commands/pane';
+import CommandTree from '../../../src/commands/tree';
+import CommandInspector from '../../../src/commands/commandinspector';
+
+describe( '<CommandsPane />', () => {
+	let editor, wrapper, element, store;
+
+	beforeEach( () => {
+		window.localStorage.clear();
+
+		element = document.createElement( 'div' );
+		document.body.appendChild( element );
+
+		return TestEditor.create( element ).then( newEditor => {
+			editor = newEditor;
+
+			store = createStore( state => state, {
+				currentEditor: editor,
+				currentEditorName: 'test-editor',
+				ui: {
+					activeTab: 'Commands'
+				},
+				commands: {
+				}
+			} );
+
+			wrapper = mount( <Provider store={store}><CommandsPane /></Provider> );
+		} );
+	} );
+
+	afterEach( () => {
+		wrapper.unmount();
+		element.remove();
+
+		return editor.destroy();
+	} );
+
+	describe( 'render()', () => {
+		it( 'renders a placeholder when no props#editor', () => {
+			store = createStore( state => state, {
+				currentEditor: null,
+				model: {
+					ui: {}
+				}
+			} );
+
+			const wrapper = mount( <Provider store={store}><CommandsPane /></Provider> );
+
+			expect( wrapper.text() ).to.match( /^Nothing to show/ );
+
+			wrapper.unmount();
+		} );
+
+		it( 'should render <Tabs>', () => {
+			const tabs = wrapper.find( 'Tabs' );
+
+			expect( tabs ).to.have.length( 1 );
+			expect( tabs.props().activeTab ).to.equal( 'Inspect' );
+		} );
+
+		it( 'should render a <CommandTree/>', () => {
+			expect( wrapper.find( CommandTree ) ).to.have.length( 1 );
+		} );
+
+		it( 'should render a <CommandInspector/>', () => {
+			expect( wrapper.find( CommandInspector ) ).to.have.length( 1 );
+		} );
+	} );
+} );
