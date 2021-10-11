8e33bb0988b344466f4eb3aa4221b48091b3230e
przemyslaw-zan <69513154+przemyslaw-zan@users.noreply.github.com>
2021-10-11 10:33:05 +0200

    Apply suggestions from code review
    Co-authored-by: Kamil Piechaczek <pomek@users.noreply.github.com>

--- tests/inspector/ui.js
+++ tests/inspector/ui.js
@@ -118,14 +118,14 @@ describe( '<InspectorUI />', () => {
 			expect( document.body.classList.contains( 'ck-inspector-body-collapsed' ) ).to.be.true;
 		} );
 
-		it( 'body element should have the correct bottom margin the when expanded', () => {
+		it( 'should define the margin-bottom on the `body` element with the height of the inspector container (non-collapsed)', () => {
 			expect( document.body.classList.contains( 'ck-inspector-body-expanded' ) ).to.be.true;
 			expect( document.body.classList.contains( 'ck-inspector-body-collapsed' ) ).to.be.false;
 
 			expect( window.getComputedStyle( document.body ).marginBottom ).to.equal( '123px' );
 		} );
 
-		it( 'body element should have the correct bottom margin the when collapsed', () => {
+		it( 'should define the margin-bottom on the `body` element with the height of the inspector container (collapsed)', () => {
 			store.dispatch( {
 				type: 'testAction',
 				state: {
