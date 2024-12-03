051f7f3b8382a0f047f6a73217352e607e4d049b
Kamil Piechaczek <pomek@users.noreply.github.com>
2024-12-03 14:16:13 +0100

    Updated license.

--- src/schema/data/actions.js
+++ src/schema/data/actions.js
@@ -1,6 +1,6 @@
 /**
  * @license Copyright (c) 2003-2024, CKSource Holding sp. z o.o. All rights reserved.
- * For licensing, see LICENSE.md.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-licensing-options
  */
 
 export const SET_SCHEMA_CURRENT_DEFINITION_NAME = 'SET_SCHEMA_CURRENT_DEFINITION_NAME';
