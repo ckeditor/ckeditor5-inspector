75d4e534db551e36ad6826784fafa44ed1b3e30b
Przemysław Zań <przemyslaw.zan@gmail.com>
2025-06-30 17:40:10 +0200

    Switched to new changelog.

--- scripts/postinstall.mjs
+++ scripts/postinstall.mjs
@@ -5,12 +5,14 @@
 
 'use strict';
 
-const path = require( 'path' );
-const fs = require( 'fs' );
-const ROOT_DIRECTORY = path.join( __dirname, '..' );
+import path from 'upath';
+import fs from 'fs';
+import { ROOT_DIRECTORY } from './utils/constants.mjs';
 
 // When installing a repository as a dependency, the `.git` directory does not exist.
 // In such a case, husky should not attach its hooks as npm treats it as a package, not a git repository.
 if ( fs.existsSync( path.join( ROOT_DIRECTORY, '.git' ) ) ) {
-	require( 'husky' ).install();
+	const { default: husky } = await import( 'husky' );
+
+	husky();
 }
